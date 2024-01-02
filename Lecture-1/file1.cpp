83dd74b4f599fb597683d9bbd281fe1fecb74d9c
Aditya Pandey <aditya.pandey2022@vitstudent.ac.in>
2024-01-02 22:49:00 +0530

    Lec-1 Done

---
+++ Lecture-1/file1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // Covered the following topics : 
+    // Basic input and output
+    // Datatypes
+    // Conditional Statements
+    // Looping Statements
+    // Array (Basic)
+    // Strings (Basic)
+    // Functions
+    return 0;
+}
